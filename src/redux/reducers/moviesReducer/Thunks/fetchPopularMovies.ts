319a0b57d36680cc91e742acfa3ce59f3b556000
artem_priadkin <artem_priadkin@epam.com>
2023-02-02 20:33:38 +0100

    add fetchPopularMovies thunk

---
+++ src/redux/reducers/moviesReducer/Thunks/fetchPopularMovies.ts
@@ -0,0 +1,19 @@
+import {API} from 'src/api/API';
+import type {AppThunk} from 'src/redux/store';
+
+import {setMoviesArray, setTotalPages} from '../moviesSlice';
+
+const MAX_NUMBER_OF_PAGES = 500;
+
+const normalizeTotalPages = (totalPages: number) => (
+    totalPages > MAX_NUMBER_OF_PAGES ? MAX_NUMBER_OF_PAGES : totalPages
+);
+
+export const fetchPopularMovies = (): AppThunk => async dispatch => {
+    const moviesList = await API.getPopularMovies();
+    moviesList?.results && dispatch(setMoviesArray(moviesList.results));
+    if (moviesList?.total_pages) {
+        const normalizedTotalPages = normalizeTotalPages(moviesList?.total_pages);
+        dispatch(setTotalPages(normalizedTotalPages));
+    }
+};
