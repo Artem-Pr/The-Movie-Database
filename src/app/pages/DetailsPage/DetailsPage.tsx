a654bac02ad81117115d9bb5c719f3af3d690fea
artem_priadkin <artem_priadkin@epam.com>
2023-02-04 19:46:06 +0100

    add placeHolders and Skeletons to the DetailsPage

--- src/app/pages/DetailsPage/DetailsPage.tsx
+++ src/app/pages/DetailsPage/DetailsPage.tsx
@@ -1,14 +1,16 @@
-import React, {useEffect} from 'react';
+import React, {SyntheticEvent, useEffect, useState} from 'react';
 import {useSelector} from 'react-redux';
 import {useNavigate, useParams} from 'react-router-dom';
 
 import {
     Button,
     Descriptions,
+    Skeleton,
     Typography,
 } from 'antd';
 import cn from 'classnames';
 
+import imgPlaceholder from 'src/assets/images/image-placeholder.svg';
 import {fetchMovieDetails} from 'src/redux/reducers/movieDetailsReducer/thunks';
 import {getMovieDetails, getMovieProperties} from 'src/redux/selectors/movieDetailsSelectors';
 import {useAppDispatch} from 'src/redux/store';
@@ -22,6 +24,10 @@ const {
     Link,
 } = Typography;
 
+const handleImageOnLoad = (loading: boolean) => (event: SyntheticEvent<HTMLImageElement>) => {
+    !loading && event.currentTarget.classList.remove('d-none');
+};
+
 export const DetailsPage = () => {
     const navigate = useNavigate();
     const dispatch = useAppDispatch();
@@ -30,13 +36,23 @@ export const DetailsPage = () => {
         posterPath,
         overview,
         homepage,
+        id,
     } = useSelector(getMovieDetails);
     const movieProperties = useSelector(getMovieProperties);
-    const {id} = useParams();
+    const {id: paramId} = useParams();
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        id && dispatch(fetchMovieDetails(id));
-    }, [dispatch, id]);
+        const sameMovie = id === Number(paramId as string);
+
+        const fetchDetails = async () => {
+            setLoading(true);
+            !sameMovie && await dispatch(fetchMovieDetails(paramId as string));
+            setLoading(false);
+        };
+
+        paramId && fetchDetails();
+    }, [dispatch, id, paramId]);
 
     const handleBackButton = () => {
         navigate(-1);
@@ -54,51 +70,70 @@ export const DetailsPage = () => {
                 </Button>
 
                 <img
-                    className={styles.image}
+                    className={cn(styles.image, 'd-none')}
                     alt={title}
                     src={posterPath}
+                    onLoad={handleImageOnLoad(loading)}
                 />
+                <div className={styles.imgPlaceholder}>
+                    <img
+                        src={imgPlaceholder}
+                        alt={title}
+                    />
+                </div>
 
                 <div className={styles.titleProp}>
-                    <Title
-                        className="mt-0"
-                        level={2}
-                    >
-                        {title}
-                    </Title>
-
-                    <Descriptions
-                        column={1}
-                        labelStyle={{fontWeight: 'bold'}}
-                    >
-                        {movieProperties.map(({name, value}) => (
-                            <Descriptions.Item
-                                key={name}
-                                label={name}
+                    {loading
+                        ? <Skeleton.Input className="m-5" active />
+                        : (
+                            <Title
+                                className="mt-0"
+                                level={2}
                             >
-                                <Text type="secondary">{value}</Text>
-                            </Descriptions.Item>
-                        ))}
-                    </Descriptions>
+                                {title}
+                            </Title>
+                        )}
+
+                    {loading
+                        ? <Skeleton paragraph={{rows: 6}} active />
+                        : (
+                            <Descriptions
+                                column={1}
+                                labelStyle={{fontWeight: 'bold'}}
+                            >
+                                {movieProperties.map(({name, value}) => (
+                                    <Descriptions.Item
+                                        key={name}
+                                        label={name}
+                                    >
+                                        <Text type="secondary">{value}</Text>
+                                    </Descriptions.Item>
+                                ))}
+                            </Descriptions>
+                        )}
                 </div>
 
-                <div className={styles.description}>
-                    <Title level={3}>
-                        Overview
-                    </Title>
-                    <Paragraph>
-                        {overview}
-                    </Paragraph>
-                    <div className="d-flex gap-10">
-                        <Text>Homepage:</Text>
-                        <Link
-                            href={homepage}
-                            target="_blank"
-                        >
-                            {homepage}
-                        </Link>
-                    </div>
-                </div>
+                {loading
+                    ? <Skeleton className={styles.description} active />
+                    : (
+                        <div className={styles.description}>
+                            <Title level={3}>
+                                Overview
+                            </Title>
+                            <Paragraph>
+                                {overview}
+                            </Paragraph>
+                            <div className="d-flex gap-10">
+                                <Text>Homepage:</Text>
+                                <Link
+                                    href={homepage}
+                                    target="_blank"
+                                >
+                                    {homepage}
+                                </Link>
+                            </div>
+                        </div>
+                    )}
             </div>
         </div>
     );
